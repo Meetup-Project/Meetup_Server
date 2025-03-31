28afb6003ff7ef354c545d672865fb039b6fe329
Hannah <sunday0206@dsm.hs.kr>
2025-03-31 23:40:02 +0900

    CustomException을 위한 기초적인 클래스 추가

---
+++ src/main/java/com/work/meetup/exception/customException/ErrorCode.java
@@ -0,0 +1,17 @@
+package com.work.meetup.exception.customException;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum ErrorCode {
+    USER_NOT_FOUND(400,"일치하는 유저를 찾을 수 없습니다."),
+    MEETING_NOT_FOUND(400,"일치하는 모임을 찾을 수 없습니다."),
+    MEMBER_NOT_FOUND(400,"일치하는 멤버를 찾을 수 없습니다."),
+    UNAUTHORIZED(401, "권한이 없습니다."),
+    INTERNAL_SERVER_ERROR(500, "서버에서 오류가 발생하였습니다.");
+
+    private final int httpStatus;
+    private final String message;
+}
