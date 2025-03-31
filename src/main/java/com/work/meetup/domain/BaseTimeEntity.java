e41be26e87fcb0e6e2295ea148738dee214884b3
Hannah <sunday0206@dsm.hs.kr>
2025-03-31 23:35:02 +0900

    BaseTimeEntity 클래스, Role 클래스 추가

---
+++ src/main/java/com/work/meetup/domain/BaseTimeEntity.java
@@ -0,0 +1,21 @@
+package com.work.meetup.domain;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+    @CreatedDate
+    private LocalDate createDate; //Entity 생성 및 저장 시 생성일시 자동 저장
+
+    @LastModifiedDate
+    private LocalDate updateDate; //조회한 Entity의 값 변경 시 수정일시 자동 저장
+}
---
+++ src/main/java/com/work/meetup/domain/enums/Role.java
@@ -0,0 +1,7 @@
+package com.work.meetup.domain.enums;
+
+public enum Role {
+    MEMBER,
+    ADMIN,
+    OWNER;
+}
